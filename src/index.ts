e0a29ed6f14414563a913a4120a073d9e6c4230c
eduardo ruiz <eduardo.ruiz2@vwcredit.com>
2023-01-12 09:44:59 -0600

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.listen(4000, () => {
+  console.log('Server running on port 4000');
+})
+
+app.get('/', (req, res) => {
+  res.send('test');
+})
